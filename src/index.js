c8fe049174ac7a31375a4de9513370b4051c53e8
finestra771 <minjokwindows5@gmail.com>
2023-08-01 16:01:09 +0900

    dev

--- src/index.js
+++ src/index.js
@@ -3,21 +3,19 @@
 // //
 //messenger app like whatsapp web with createRoot: index.js
 import React from "react";
-import ReactDOM from "react-dom";
-import { createRoot } from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
 // import React from "react";
 // import ReactDOM from "react-dom";
 // import "./index.css";
 // import App from "./App";
-const root = createRoot(document.getElementById("root"));
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-//
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+// //
